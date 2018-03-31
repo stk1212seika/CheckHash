37c832908b253e669f324144eb30bbae5d191617
stk1212seika <stk1212seika@users.noreplay.github.com>
2018-03-31 21:27:30 +0900

    Read file

--- CheckHash/Program.cs
+++ CheckHash/Program.cs
@@ -10,6 +10,7 @@ namespace CheckHash
 	{
 		static void Main(string[] args)
 		{
+
 		}
 	}
 }
---
+++ CheckHash/ReadHash.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Security.Cryptography;
+
+namespace CheckHash
+{
+	class ReadHash
+	{
+		public ReadHash(string FilePath)
+		{
+			var m00 = File.ReadAllBytes(FilePath + "m00.sav");
+			var r00 = File.ReadAllBytes(FilePath + "r00.sav");
+			var s00 = File.ReadAllBytes(FilePath + "s00.sav");
+
+			// 長さを変えて探索
+			for (int length = 0; length < 64; length++) {
+				
+			}
+		}
+
+	}
+}
